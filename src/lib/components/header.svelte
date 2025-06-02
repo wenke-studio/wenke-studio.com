721c9a58dffaf97ed0208e5a3cd960e131199592
wenke.lai <wenke.studio@gmail.com>
2025-06-02 08:04:03 +0800

    feat(header): add NavHamburger component and reorganize NavUl placement

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <script lang="ts">
-    import { Heading, Navbar, NavBrand, NavLi, NavUl } from "flowbite-svelte";
+    import { Heading, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from "flowbite-svelte";
     import Logo from "./icon/logo.svelte";
     import SearchDrawer from "./search-drawer.svelte";
     import SearchModal from "./search-modal.svelte";
@@ -28,20 +28,21 @@
             </Heading>
         </NavBrand>
 
-        <NavUl class="flex-grow">
-            {#each links as link}
-                <NavLi href={link.href}>{link.label}</NavLi>
-            {/each}
-        </NavUl>
-
         <div class="flex md:order-2">
             <div class=" flex gap-2 md:hidden">
                 <SearchDrawer {links} />
+                <NavHamburger />
             </div>
 
             <div class="hidden md:block">
                 <SearchModal {links} />
             </div>
         </div>
+
+        <NavUl class="flex-grow">
+            {#each links as link}
+                <NavLi href={link.href}>{link.label}</NavLi>
+            {/each}
+        </NavUl>
     </Navbar>
 </header>
