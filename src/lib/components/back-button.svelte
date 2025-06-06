f382ec563f1772e5511f20ce248bed849e44dd9e
wenke.lai <wenke.studio@gmail.com>
2025-06-06 16:32:52 +0800

    feat(error): add custom 404 page with back button and illustration

---
+++ src/lib/components/back-button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { Button } from "flowbite-svelte";
+
+    const goBack = () => {
+        if (browser) window.history.back();
+    };
+</script>
+
+<Button outline onclick={goBack}>上一頁</Button>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-    import { page } from "$app/state";
+    import UndrawPageNotFound from "$lib/assets/images/undraw_page-not-found_6wni.svelte";
+    import BackButton from "$lib/components/back-button.svelte";
+    import Main from "$lib/components/main.svelte";
+    import { Heading } from "flowbite-svelte";
 </script>
 
-<h1>{page.status}: {page.error?.message}</h1>
+<Main>
+    <div class="my-12">
+        <Heading tag="h3" class="mb-8 text-center">Page Not Found</Heading>
+        <div class="mx-auto max-w-lg">
+            <UndrawPageNotFound />
+            <div class="my-8">
+                <BackButton />
+            </div>
+        </div>
+    </div>
+</Main>
