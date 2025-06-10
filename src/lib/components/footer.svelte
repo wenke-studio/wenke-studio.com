5e4cb74a134951aa33fb097663d31a41618687ae
wenke.lai <wenke.studio@gmail.com>
2025-06-10 10:30:08 +0800

    chore(footer): clean up unused imports and comment out social media icons

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,25 +1,14 @@
 <script>
-    import {
-        A,
-        DarkMode,
-        Footer,
-        FooterBrand,
-        FooterCopyright,
-        FooterIcon,
-        FooterLink,
-        FooterLinkGroup,
-        P
-    } from "flowbite-svelte";
-    import { DiscordSolid, FacebookSolid, GithubSolid, TwitterSolid } from "flowbite-svelte-icons";
+    import { A, DarkMode, Footer, FooterBrand, FooterCopyright, P } from "flowbite-svelte";
 </script>
 
 <Footer footerType="socialmedia">
     <div class="container mx-auto md:flex md:justify-between">
-        <div class="mb-6 md:mb-0">
+        <div class="mb-6 flex items-center gap-4 md:mb-0">
             <FooterBrand href="/" name="Wenke" />
             <DarkMode />
         </div>
-        <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
+        <!-- <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
             <div>
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                     Resources
@@ -47,7 +36,7 @@
                     <FooterLink liClass="mb-4" href="/">Terms & Conditions</FooterLink>
                 </FooterLinkGroup>
             </div>
-        </div>
+        </div> -->
     </div>
     <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
     <div class="container mx-auto sm:flex sm:items-center sm:justify-between">
@@ -58,26 +47,11 @@
             </P>
         </div>
         <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center rtl:space-x-reverse">
-            <FooterIcon href="/">
-                <FacebookSolid
-                    class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
-                />
-            </FooterIcon>
-            <FooterIcon href="/">
+            <!-- <FooterIcon href="/">
                 <DiscordSolid
                     class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
                 />
-            </FooterIcon>
-            <FooterIcon href="/">
-                <TwitterSolid
-                    class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
-                />
-            </FooterIcon>
-            <FooterIcon href="/">
-                <GithubSolid
-                    class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
-                />
-            </FooterIcon>
+            </FooterIcon> -->
         </div>
     </div>
 </Footer>
