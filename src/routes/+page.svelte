1bb19cf43f72a5487855bd619f53aebd7838cc5c
wenke.lai <wenke.studio@gmail.com>
2025-06-10 14:56:57 +0800

    feat(page): update card styles  image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
     import Main from "$lib/components/main.svelte";
     import Sidebar from "$lib/components/sidebar.svelte";
+    import { cn } from "$lib/utils";
     import { A, Badge, Card, Heading, Hr, P } from "flowbite-svelte";
 
     const { data } = $props();
 </script>
+
+{#snippet featuredCard(article, cardClass, imgClass)}
+    <Card
+        href="/posts/{article.title}"
+        class={cn(cardClass, "")}
+        size="xl"
+        img="/images/{article.image}"
+        {imgClass}
+    >
+        <div class="flex flex-col gap-2 p-2">
+            <Heading tag="h6">{article.title}</Heading>
+            <P>{article.description}</P>
+        </div>
+    </Card>
+{/snippet}
 
 <Main>
     <section id="Editor's Pick" class="relative w-full">
@@ -12,22 +28,15 @@
             <Heading tag="h5">作者精選</Heading>
             <A href="/posts/">更多</A>
         </hgroup>
-        <div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:grid-rows-2">
-            <Card href={`/posts/${data.featured[0]?.title}`} class="row-span-2 h-96 p-4" size="xl">
-                <div class="flex flex-col gap-2">
-                    <Heading tag="h5">{data.featured[0]?.title}</Heading>
-                    <!-- <Secondary>{data.featured[0].c.label}</Secondary> -->
-                </div>
-            </Card>
 
-            {#each data.featured.slice(1, 2) as article}
-                <Card href={`/posts/${article.title}`} class="hidden p-4 lg:block" size="xl">
-                    <div class="flex flex-col gap-2">
-                        <Heading tag="h5">{article.title}</Heading>
-                        <!-- <Secondary>{article.category.label}</Secondary> -->
-                    </div>
-                </Card>
-            {/each}
+        <div class="grid h-[512px] grid-cols-3 grid-rows-2 gap-4">
+            {@render featuredCard(
+                data.featured[0],
+                "col-span-2 row-span-2 h-full",
+                "object-cover h-4/5"
+            )}
+            {@render featuredCard(data.featured[1], "col-start-3", "object-cover h-1/2")}
+            {@render featuredCard(data.featured[2], "col-start-3", "object-cover h-1/2")}
         </div>
     </section>
 
@@ -41,16 +50,17 @@
         <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
             {#each data.popular.slice(0, 4) as article, index}
                 <Card
-                    href={`/posts/${article.title}`}
-                    class="h-fit p-4"
+                    href="/posts/{article.title}"
+                    class=""
                     size="xl"
                     horizontal={index >= 2}
+                    img="/images/{article.image}"
+                    imgClass={cn("object-cover", index >= 2 && "w-32!")}
                 >
-                    <div class="flex flex-col gap-2">
+                    <div class="flex flex-col gap-2 p-2">
                         <Heading tag="h5">{article.title}</Heading>
-                        <!-- <Secondary>{article.group}</Secondary> -->
+                        <P>{article.description}</P>
                     </div>
-                    <!-- <P class="min-h-64">{article.description}</P> -->
                 </Card>
             {/each}
         </div>
@@ -65,19 +75,20 @@
         </hgroup>
         <div class="grid grid-cols-1 gap-4">
             {#each data.articles.slice(0, 4) as item}
-                <Card href={`/posts/${item.title}`} class="h-fit p-4" size="xl">
-                    <div class="flex flex-col justify-between gap-2 md:flex-row">
-                        <div class="">
-                            <Heading tag="h5">{item.title}</Heading>
-                            <P>{item.description}</P>
-                            <div class="mt-4 flex gap-2">
-                                {#each item.hashtags as hashtag}
-                                    <Badge>#{hashtag.label}</Badge>
-                                {/each}
-                            </div>
-                        </div>
-                        <div>
-                            <div class="h-16 w-16 bg-gray-300"></div>
+                <Card
+                    href={`/posts/${item.title}`}
+                    class="flex-row-reverse overflow-hidden"
+                    size="xl"
+                    img="/images/{item.image}"
+                    imgClass="object-cover h-auto w-32 rounded-none border-0"
+                >
+                    <div class="grow p-4">
+                        <Heading tag="h5">{item.title}</Heading>
+                        <P>{item.description}</P>
+                        <div class="mt-4 flex gap-2">
+                            {#each item.hashtags as hashtag}
+                                <Badge>#{hashtag.label}</Badge>
+                            {/each}
                         </div>
                     </div>
                 </Card>
